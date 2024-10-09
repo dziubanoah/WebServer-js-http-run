ff775201c6b55e7553a9f979bd83cb32c84e94ea
dziubanoah <dziubanoah@gmail.com>
2024-10-09 12:06:56 +0200

    v1.6

---
+++ jtest/test.java
@@ -0,0 +1,27 @@
+package jtest;
+import java.awt.*;
+import java.lang.classfile.instruction.*;
+import java.rmi.ConnectIOException;
+import javax.swing.*;
+
+public class test {
+    public static void main(String[] args) {
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+    
+        JFrame fenster = new JFrame("Onlinebanking");
+        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenster.setSize(800, 500);
+
+        ImageIcon img = new ImageIcon("coin.jpg");
+        fenster.setIconImage(img.getImage());
+
+        fenster.add(new JLabel(new ImageIcon("frontend.PNG")));
+
+        fenster.setVisible(true);
+    
+        }    
+    });
+}
+}
