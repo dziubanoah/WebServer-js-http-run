0ebe15da5635fcf138e1d08b5d7ed216dd6eb87d
dziubanoah <dziubanoah@gmail.com>
2024-10-09 10:40:43 +0200

    v.1.4 inkl. Java Frame

---
+++ Server scripts/fenster.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.lang.classfile.instruction.*;
+import java.rmi.ConnectIOException;
+import javax.swing.*;
+
+
+public class fenster {
+    public static void main(String[] args) {
+        
+        JFrame fenster = new JFrame("Onlinebanking");
+        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenster.setSize(800, 500);
+
+        ImageIcon img = new ImageIcon("coin.jpg");
+        fenster.setIconImage(img.getImage());
+
+        fenster.add(new JLabel(new ImageIcon("frontend.PNG")));
+
+        fenster.setVisible(true);
+        
+        
+    }
+}
