255f2ee32e7b4392613ee6bfa200f40ddc1bfcd8
dziubanoah <dziubanoah@gmail.com>
2024-10-09 11:47:43 +0200

    v1.5

--- Server scripts/fenster.java
+++ Server scripts/fenster.java
@@ -6,18 +6,22 @@ import javax.swing.*;
 
 public class fenster {
     public static void main(String[] args) {
-        
-        JFrame fenster = new JFrame("Onlinebanking");
-        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenster.setSize(800, 500);
 
-        ImageIcon img = new ImageIcon("coin.jpg");
-        fenster.setIconImage(img.getImage());
-
-        fenster.add(new JLabel(new ImageIcon("frontend.PNG")));
-
-        fenster.setVisible(true);
+            javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
         
+            JFrame fenster = new JFrame("Onlinebanking");
+            fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            fenster.setSize(800, 500);
+
+            ImageIcon img = new ImageIcon("coin.jpg");
+            fenster.setIconImage(img.getImage());
+
+            fenster.add(new JLabel(new ImageIcon("frontend.PNG")));
+
+            fenster.setVisible(true);
         
+            }    
+        });
     }
 }
