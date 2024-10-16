8947f82a44127f71574365c84ab90cf7ba7e18fa
dziubanoah <dziubanoah@gmail.com>
2024-10-16 11:17:16 +0200

    v2.5

--- Submit for Hacker_1.html/node js backend/main_node.js
+++ Submit for Hacker_1.html/node js backend/main_node.js
@@ -1,13 +1,11 @@
 var http = require('http');
 var url = require("url");
+var fs = require("fs")
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.mounth;
-  res.write(txt)
-  res.end();
-  exports.myDateTime = function() {
-    return Date();
-  }
+    fs.readFile("index.html", function(err, data) {
+        res.writeHead(200, {"content-type": "text/html"});
+        res.write(data);
+        return res.end();
+    });
 }).listen(8080);
