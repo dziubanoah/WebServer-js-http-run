a03e5a6bd197b161b24233957962530fa90b02c4
dziubanoah <145217862+dziubanoah@users.noreply.github.com>
2024-10-17 11:46:48 +0200

    v3.1

--- Server scripts/script_mit_log.php
+++ Server scripts/script_mit_log.php
@@ -29,21 +29,3 @@ $logEintrag = "Team: $team, Datum: $datum, Zeit: $zeit\n";
 //damit man nicht so dumm ist wie ich: DIE log.txt SOLLTE IM SELBEN VERZEICHNISS EXISTIEREN  ^^
 file_put_contents('log.txt', $logEintrag, FILE_APPEND);
 ?>
-<?php
-header('Content-Type: application/json');
-
-// Nimmt die Rohdaten des POST-Requests (JSON) entgegen
-$json = file_get_contents('php://input');
-
-// Konvertiert JSON in ein assoziatives Array
-$data = json_decode($json, true);
-
-if (isset($data['inputname'])) {
-    $inputname = $data['inputname'];
-    file_put_contents('log.txt', date('Y-m-d H:i:s') . " - Empfangen: " . $inputname . PHP_EOL, FILE_APPEND);
-} else {
-    file_put_contents('log.txt', date('Y-m-d H:i:s') . " - Fehler: 'inputname' nicht gefunden" . PHP_EOL, FILE_APPEND);
-}
-echo"Script 2 klappt";
-
-?>
