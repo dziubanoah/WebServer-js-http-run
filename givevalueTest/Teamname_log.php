8baada48e59d747c034aa1ea737f88edf3ee7ca0
dziubanoah <dziubanoah@gmail.com>
2024-10-17 16:46:28 +0200

    v3.2

---
+++ givevalueTest/Teamname_log.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($_POST['inputValue'])) {
+    $inputname = $_POST['inputValue'];
+    file_put_contents('log.txt', date('Y-m-d H:i:s') . " - Empfangen: " . $inputname . PHP_EOL, FILE_APPEND);
+} else {
+    file_put_contents('log.txt', date('Y-m-d H:i:s') . " - Fehler: 'inputValue' nicht gefunden" . PHP_EOL, FILE_APPEND);
+}
+echo"Log script works" + $inputname;
+?>
