3bc429820116e92172153def9be5d5299a408f66
dziubanoah <145217862+dziubanoah@users.noreply.github.com>
2024-10-17 16:59:40 +0200

    v3.3

---
+++ givevalueTest/Keks.js
@@ -0,0 +1,26 @@
+
+var input = document.getElementById("NameInput");
+let submit = document.getElementById("submit");  // Korrekt: 'submit', nicht 'Sumit'
+function getClientId() {
+    let clientId = document.cookie.replace(/(?:(?:^|.*;\s*)clientId\s*=\s*([^;]*).*$)|^.*$/, "$1");
+
+    if (!clientId) {
+        clientId = 'client-' + Date.now() + '-' + Math.random().toString(36);
+        document.cookie = `clientId=${clientId}; path=/; max-age=31536000`; // 1 Jahr gültig
+    }
+    
+    return clientId;
+}
+
+let clientId = getClientId();
+console.log("Client ID:", clientId);
+
+if (clientId === 'client-1728984694739-0.j354wprsbq') {
+    input.style.display= "block"; 
+    submit.style.display= "block";
+
+}
+else (
+    console.log("Die coockie ID aus main.js ist nicht korrekt")
+);
+    window.alert("keks check durchgeführt");
